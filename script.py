05a5fe25342e94e3d6165b7aa8bc7ff5251b282c
Erick Delfin <erickdelfinmolina@gmail.com>
2018-05-27 05:30:25 -0700

    Add go_to_grades function and use os.environ for user/pw

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,34 +15,28 @@ action = ActionChains(driver)
 def portal_login(email, password):
     driver.get("https://buhos.uson.mx/web/apps/portalAlumnos/index.php/login")
 
-    driver.find_element_by_id("ext-gen1016").send_keys(email)
-    driver.find_element_by_id("ext-gen1022").send_keys(password)
-    driver.find_element_by_id("button-1018-btnEl").click()
+    driver.find_element_by_xpath('//*[@id="ext-gen1016"]').send_keys(email)
+    driver.find_element_by_xpath('//*[@id="ext-gen1022"]').send_keys(password)
+    driver.find_element_by_xpath('//*[@id="button-1018-btnEl"]').click()
 
 
 def go_to_grades():
     # 'Mis Materiales'
-    driver.find_element_by_id("1433").click()
-    grade_link_id = "ext-gen1042"
-    wait.until(EC.presence_of_element_located((By.ID, grade_link_id)))
+    driver.find_element_by_xpath('//*[@id="1433"]').click()
 
-    time.sleep(5)
-    grade_link = driver.find_element_by_id("ext-gen1042")
-    hover = action.move_to_element(grade_link)
-    print("HOVER")
-    hover.perform()
-    time.sleep(5)
+    # Give time for iframe to load
+    time.sleep(3)
+    iframe = driver.find_element_by_xpath('//*[@id="ext-gen1036"]')
+    driver.switch_to.frame(iframe)
 
-    # 'Calificaciones'
-
-
-    # grade_link.click()
-    # driver.find_element_by_id("ext-gen1041").click()
+    # Calificaciones
+    driver.find_element_by_xpath('//*[@class="thumb-wrap"]').click()
     wait.until(EC.title_contains("UniSon"))
 
 
 def check_for_grade():
-
+    print('CHECKING GRADE...')
+    time.sleep(5)
     pass
 
 
@@ -49,13 +44,14 @@ def refresh_page_every(seconds, loops):
     for _ in range(loops):
         wait.until(EC.title_contains("UniSon"))
         go_to_grades()
-        print('REFRESHED')
-        time.sleep(seconds)
+        check_for_grade()
+        print('REFRESHING')
         driver.refresh()
+        time.sleep(seconds)
 
 
 def main():
-    portal_login(email, password)
+    portal_login(os.environ['GRADE_USER'], os.environ['GRADE_PASSWORD'])
     print("Successfully logged in!")
     refresh_page_every(10, 5)
     print('============== FINISHED ==============')
