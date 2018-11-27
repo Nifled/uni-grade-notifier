27e49b6c0f1a61927c08f6fe53fd9ad28edeefb8
nifled <erickdelfinmolina@gmail.com>
2018-11-27 13:11:30 -0700

    Set up new script with requests lib using sessions

--- script.py
+++ script.py
@@ -1,61 +1,31 @@
-import os
 import time
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
+import json
+import requests
 
+LOGIN_URL = 'https://buhos.uson.mx/web/apps/portalAlumnos/index.php/auth/login/entrar'
+# GRADES_URL = 'https://buhos.uson.mx/web/apps/portalAlumnos/index.php/calificaciones'
+GRADES_URL = 'https://buhos.uson.mx/portalalumnos/obtener/calificacionesFinalesEstudiante'
+DATA_URL = 'https://buhos.uson.mx/web/apps/portalAlumnos/index.php/auth/sesion/datos_alumno'
+CYCLE_URL = 'https://buhos.uson.mx/web/apps/portalAlumnos/index.php/horario/ciclosActivos'
 
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 10)
-action = ActionChains(driver)
+PORTAL_USER = ''
+PORTAL_PW = ''
 
+form_data_login = {'u': PORTAL_USER, 'p': PORTAL_PW}
 
-def portal_login(email, password):
-    driver.get("https://buhos.uson.mx/web/apps/portalAlumnos/index.php/login")
+with requests.Session() as s:
+    s.post(LOGIN_URL, data=form_data_login)
 
-    driver.find_element_by_xpath('//*[@id="ext-gen1016"]').send_keys(email)
-    driver.find_element_by_xpath('//*[@id="ext-gen1022"]').send_keys(password)
-    driver.find_element_by_xpath('//*[@id="button-1018-btnEl"]').click()
+    # Cycle id for grades
+    cycle = s.post(CYCLE_URL)
+    id_cycle = cycle.json().get('data')[0].get('id_ciclo')
 
+    # Student id for grades
+    info = s.post(DATA_URL)
+    id_student = info.json().get('data').get('niveles')[0].get('ide')
 
-def go_to_grades():
-    # 'Mis Materiales'
-    driver.find_element_by_xpath('//*[@id="1433"]').click()
-
-    # Give time for iframe to load
-    time.sleep(3)
-    iframe = driver.find_element_by_xpath('//*[@id="ext-gen1036"]')
-    driver.switch_to.frame(iframe)
-
-    # Calificaciones
-    driver.find_element_by_xpath('//*[@class="thumb-wrap"]').click()
-    wait.until(EC.title_contains("UniSon"))
-
-
-def check_for_grade():
-    print('CHECKING GRADE...')
-    time.sleep(5)
-    pass
-
-
-def refresh_page_every(seconds, loops):
-    for _ in range(loops):
-        wait.until(EC.title_contains("UniSon"))
-        go_to_grades()
-        check_for_grade()
-        print('REFRESHING')
-        driver.refresh()
-        time.sleep(seconds)
-
-
-def main():
-    portal_login(os.environ['GRADE_USER'], os.environ['GRADE_PASSWORD'])
-    print("Successfully logged in!")
-    refresh_page_every(10, 5)
-    print('============== FINISHED ==============')
-
-
-if __name__ == '__main__':
-    main()
+    grades = s.post(GRADES_URL, data={
+        'idEstudiante': id_student,
+        'idCiclo': id_cycle,
+    })
+    print(json.dumps(grades.json()))
