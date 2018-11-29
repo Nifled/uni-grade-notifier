3e36412f83c512abe13247c940b4c61af36f96a2
nifled <erickdelfinmolina@gmail.com>
2018-11-29 00:54:03 -0700

    Update job script

--- sample-script.sh
+++ sample-script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# env vars
+
+# env vars (modify!)
 export USER_EMAIL='example@mail.com'
 export PORTAL_USER='a214215385@alumnos.unison.mx'
 export PORTAL_PW='your-password'
 export SENDGRID_API_KEY='create-free-sendgrid-account-in-5-mins-for-apikey'
 
-VENV_PY="/home/nifled/.local/share/virtualenvs/uni-grade-notifier-4pX91mei/bin/python" # Make sure packages are installed
-PROJECT="/home/nifled/erick/uni-grade-notifier"
 
+#########   don't modify   #########
+# paths
+VENV_PY="$PWD/.venv/bin/python"
 SCRIPT="script.py"
-cd "${PROJECT}" && "${VENV_PY}" "${SCRIPT}"
+
+# command
+"${VENV_PY}" "${SCRIPT}"
+#########   don't modify   #########
